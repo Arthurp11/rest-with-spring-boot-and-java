a41bd2be62041521517b231185c8ab2893062661
Arthur <arthurp1190@gmail.com>
2025-07-08 01:23:42 -0300

    First Steps

---
+++ rest-with-spring-boot-and-java/src/main/java/Arthurp11/com/github/Startup.java
@@ -0,0 +1,13 @@
+package Arthurp11.com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
